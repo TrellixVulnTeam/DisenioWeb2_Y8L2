3ab1427123438cf8353fc6d0a6ffed59956337df
aherreraj386 <53496837+aherreraj386@users.noreply.github.com>
2021-04-16 22:05:21 -0600

    FrontEnd 1.0

---
+++ ProyectoFinal/frontend/src/app/models/proveedore.ts
@@ -0,0 +1,17 @@
+export default class Proveedores {
+    codigo: string;
+    nombre: string;
+    primer_apellido: string;
+    segundo_apellido: string;
+    telefono_oficina: number;
+    fax: string;
+    celular: number;
+    cedula: string;
+    fecha_ingreso: number;
+    nombre_proveedor: string;
+    correo: string;
+    direccion: string;
+    nombre_contactoEmpresa: string;
+    telefono_contactoEmpresa: number;
+    direccion_empresa: string;
+}
