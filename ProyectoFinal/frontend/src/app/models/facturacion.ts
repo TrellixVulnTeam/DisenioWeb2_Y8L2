aaa1ed5f998125e3a35c155a1c773788118593a0
cris151 <65322213+cris151@users.noreply.github.com>
2021-04-16 21:45:00 -0600

    Create facturacion.ts

---
+++ ProyectoFinal/frontend/src/app/models/facturacion.ts
@@ -0,0 +1,10 @@
+export default class Facturacion{
+codigo: String;
+fecha: Date;
+descripcion: String;
+dineroentrada: Number;
+cajaapertura: Number;
+cajacierre:Number;
+restaurante: String;
+estadocuenta: Number;
+}
