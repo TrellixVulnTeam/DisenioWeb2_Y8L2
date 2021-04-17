16f0ee4e841d197c20bdecc5825687e23bd3a775
cris151 <65322213+cris151@users.noreply.github.com>
2021-04-16 21:45:11 -0600

    Create infopedido.ts

---
+++ ProyectoFinal/frontend/src/app/models/infopedido.ts
@@ -0,0 +1,12 @@
+export default class InfoPedido{
+  codigo:String;
+   numMesa: String;
+    pedidoSilla1: String;
+     pedidoSilla2: String;
+      pedidoSilla3: String;
+  pedidoSilla4: String;
+  precioSilla1: Number;
+  precioSilla2: Number;
+  precioSilla3: Number;
+  precioSilla4: Number;
+}
